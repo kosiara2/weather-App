400b77060e6a5cb5e08cbd2f151cd93c339ec159
kosiara2 <patrycja.kosierkiewicz@o2.pl>
2023-05-31 14:05:12 +0200

    catching error

--- js.js
+++ js.js
@@ -12,38 +12,54 @@ const KEY_API = "&appid=3b9ac8731082e2cb1562b3fd36f39d70";
 const METRIC_API = "&units=metric";
 
 const getWeather = () => {
+  warning.textContent = "";
+
   const city = input.value;
   const URL = LINK_API + city + KEY_API + METRIC_API;
 
-  axios.get(URL).then((res) => {
-    console.log(res.data);
-    const temp = res.data.main.temp;
-    const hum = res.data.main.humidity;
-    const status = res.data.weather[0].main;
-    const iconId = res.data.weather[0].id;
-    temperature.textContent = Math.floor(temp) + "°C";
-    cityName.textContent = res.data.name;
-    humidity.textContent = hum + "%";
-    weather.textContent = status;
+  axios
+    .get(URL)
+    .then((res) => {
+      const temp = res.data.main.temp;
+      const hum = res.data.main.humidity;
+      const status = res.data.weather[0].main;
+      const iconId = res.data.weather[0].id;
 
-    if (iconId >= 200 && iconId < 300) {
-      photo.setAttribute("src", "./img/thunderstorm.png");
-    } else if (iconId >= 300 && iconId < 400) {
-      photo.setAttribute("src", "./img/drizzle.png");
-    } else if (iconId >= 400 && iconId < 500) {
-      photo.setAttribute("src", "./img/rain.png");
-    } else if (iconId >= 500 && iconId < 600) {
-      photo.setAttribute("src", "./img/ice.png");
-    } else if (iconId >= 600 && iconId < 700) {
-      photo.setAttribute("src", "./img/fog.png");
-    } else if (iconId === 800) {
-      photo.setAttribute("src", "./img/sun.png");
-    } else if (iconId >= 800 && iconId < 900) {
-      photo.setAttribute("src", "./img/cloud.png");
-    } else {
-      photo.setAttribute("src", "./img/unknown.png");
-    }
-  });
+      temperature.textContent = Math.floor(temp) + "°C";
+      cityName.textContent = res.data.name;
+      humidity.textContent = hum + "%";
+      weather.textContent = status;
+
+      // changing photo
+      if (iconId >= 200 && iconId < 300) {
+        photo.setAttribute("src", "./img/thunderstorm.png");
+      } else if (iconId >= 300 && iconId < 400) {
+        photo.setAttribute("src", "./img/drizzle.png");
+      } else if (iconId >= 400 && iconId < 500) {
+        photo.setAttribute("src", "./img/rain.png");
+      } else if (iconId >= 500 && iconId < 600) {
+        photo.setAttribute("src", "./img/ice.png");
+      } else if (iconId >= 600 && iconId < 700) {
+        photo.setAttribute("src", "./img/fog.png");
+      } else if (iconId === 800) {
+        photo.setAttribute("src", "./img/sun.png");
+      } else if (iconId >= 800 && iconId < 900) {
+        photo.setAttribute("src", "./img/cloud.png");
+      } else {
+        photo.setAttribute("src", "./img/unknown.png");
+      }
+    })
+    .catch(() => {
+      warning.textContent = "City not found!";
+      cityName.textContent = "";
+    });
 };
 
+const enterCheck = (e) => {
+  if (e.key == "Enter") {
+    getWeather();
+  }
+};
+
+input.addEventListener("keyup", enterCheck);
 button.addEventListener("click", getWeather);
